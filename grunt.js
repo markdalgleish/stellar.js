55501e42466accbc49b2462752010673e3310601
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-05-03 16:53:54 +1000

    Use tabs in Gruntfile instead of spaces

--- grunt.js
+++ grunt.js
@@ -1,61 +1,61 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*!\n' +
+	// Project configuration.
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*!\n' +
 				' * <%= pkg.title || pkg.name %> v<%= pkg.version %>\n' +
 				' * <%= pkg.homepage %>\n' +
 				' * \n' +
-        ' * Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author.name %>\n' +
-        ' * This content is released under the <%= _.pluck(pkg.licenses, "type").join(", ") %> license<%= pkg.licenses.length === 1 ? "" : "s" %>\n' +
-        ' * <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
-        ' */',
+				' * Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author.name %>\n' +
+				' * This content is released under the <%= _.pluck(pkg.licenses, "type").join(", ") %> license<%= pkg.licenses.length === 1 ? "" : "s" %>\n' +
+				' * <%= _.pluck(pkg.licenses, "url").join(", ") %>\n' +
+				' */',
 			microbanner: '/*! <%= pkg.title || pkg.name %> v<%= pkg.version %> | Copyright <%= grunt.template.today("yyyy") %>, <%= pkg.author.name %> | <%= pkg.homepage %> | <%= _.pluck(pkg.licenses, "url").join(", ") %> */'
-    },
-    lint: {
-      files: ['grunt.js', 'src/**/*.js']
-    },
-    concat: {
-      dist: {
-        src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
-        dest: '<%= pkg.name %>.js'
-      }
-    },
-    min: {
-      dist: {
-        src: ['<banner:meta.microbanner>', '<config:concat.dist.dest>'],
-        dest: '<%= pkg.name %>.min.js'
-      }
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint'
-    },
-    jshint: {
-      options: {
-        curly: false,
-        eqeqeq: true,
-        immed: false,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
+		},
+		lint: {
+			files: ['grunt.js', 'src/**/*.js']
+		},
+		concat: {
+			dist: {
+				src: ['<banner:meta.banner>', '<file_strip_banner:src/<%= pkg.name %>.js>'],
+				dest: '<%= pkg.name %>.js'
+			}
+		},
+		min: {
+			dist: {
+				src: ['<banner:meta.microbanner>', '<config:concat.dist.dest>'],
+				dest: '<%= pkg.name %>.min.js'
+			}
+		},
+		watch: {
+			files: '<config:lint.files>',
+			tasks: 'lint'
+		},
+		jshint: {
+			options: {
+				curly: false,
+				eqeqeq: true,
+				immed: false,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true
+			},
+			globals: {
+				jQuery: true
+			}
+		},
+		uglify: {}
+	});
 
-  // Default task.
-  grunt.registerTask('default', 'lint concat min');
+	// Default task.
+	grunt.registerTask('default', 'lint concat min');
 
 };
