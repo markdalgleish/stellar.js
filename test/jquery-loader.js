470a269922d61719c562198bddb2cb858b9ab0f7
thybag <thybag@gmail.com>
2016-03-24 14:58:35 +0000

    Fix tests (use more up to date jquerys)

--- test/jquery-loader.js
+++ test/jquery-loader.js
@@ -2,7 +2,7 @@
 
 	var parts = document.location.search.slice(1).split('&'),
 		current,
-		version = '1.9.0',
+		version = '1.10.1',
 		file = 'http://code.jquery.com/jquery-git.js';
 
 	for (var i = 0; i < parts.length; i++) {
